5fe05011e51d77b4dbe0df3a83d39c8ed2a30317
OSapy <oscarsapy@gmail.com>
2021-10-20 11:27:16 +0200

    add dichotomoie

---
+++ src/dichotomy/dichotomy.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Recherche par dichotomie dans un tableau d'entiers
+ * @param array The array of values
+ * @param size_t The size of the array
+ * @param value The value to find
+ * @return The position of the value found or -1
+ */
+
+int find_by_dichotomy(int array[], int size_t, int value)
+{
+    int debut = 0, fin = size_t - 1;
+    while (debut <= fin)
+    {
+        int milieu = (debut + fin) / 2;
+        printf("Resultat array %d\n", array[milieu]);
+        if (value < array[milieu])
+        {
+            fin = milieu - 1;
+        }
+        else if (value > array[milieu])
+        {
+            debut = milieu + 1;
+        }
+        else
+        {
+            return milieu;
+        }
+    }
+    return -1;
+}
--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+#include "dichotomy/dichotomy.c"
+
 int main()
 {
-	printf("Hello World!\n");
-
-	return (0);
+	int table[] = {0, 1, 2, 3, 4, 5, 6}, taille = 7, result = 5;
+	find_by_dichotomy(table, taille, result);
 }

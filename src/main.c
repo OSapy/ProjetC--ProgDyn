8c391e324171fef5de2adbcd53249460e61715c6
OSapy <oscarsapy@gmail.com>
2021-10-22 08:17:57 +0200

    dichotomy OK

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#include "dichotomy/dichotomy.c"
+#include "dichotomy/dichotomy.h"
 
 int main()
 {
-	int table[] = {0, 1, 2, 3, 4, 5, 6}, taille = 7, result = 5;
+	int table[] = {0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11}, taille = 12, result = 8;
 	find_by_dichotomy(table, taille, result);
 }
